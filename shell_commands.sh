cd64e3fd34f5053f27fe8cf0c703fb6c6647fb02
Sean Link <sean.link@netally.com>
2021-12-15 19:27:48 -0700

    Tweaking install

--- shell_commands.sh
+++ shell_commands.sh
@@ -8,8 +8,15 @@ source ~/.nix-profile/etc/profile.d/nix.sh
 echo "Installing packges"
 xargs -d '\n' -- nix-env -iA < packages.txt
 
+echo "Adding nix zshell to list of valid shells"
+command -v zsh | sudo tee -a /etc/shells
+
 echo "Changing shell"
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $USER
+
+echo "Installing vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Installing nvim pluggins"
-nvim +PlugUpgrade +PlugInstall +PlugUpdate
+nvim +PlugUpgrade +PlugInstall +PlugUpdate +qa
