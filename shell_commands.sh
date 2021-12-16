65e0de7072cad6731e660839959bde9d5cdb1c57
Sean Link <sean.link@netally.com>
2021-12-15 20:02:44 -0700

    Install tweaks

--- shell_commands.sh
+++ shell_commands.sh
@@ -14,6 +14,9 @@ command -v zsh | sudo tee -a /etc/shells
 echo "Changing shell"
 sudo chsh -s $(which zsh) $USER
 
+echo "Installing shell pluggins"
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
 echo "Installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
